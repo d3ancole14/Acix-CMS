8776ba94950a3bdf9e1cb58fdb60b98ebb7f3da9
Dean Cole <d3ancole14@icloud.com>
2014-10-14 16:09:46 +0100

    Initial File Structure

---
+++ app/admin/config/database.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Dean Cole
+ * Date: 14/10/2014
+ * Time: 16:04
+ */
+
+$host = "localhost";
+$user = "root";
+$pass = "password";
+$db = "acixcms";
+
+mysql_connect($host,$user,$pass)or die(mysql_error());
+mysql_select_db($db)or die(mysql_error());
